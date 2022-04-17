d10da1fe14a1f2b51bbab4535afe72cd53420913
timokhin Iliya <timokhin813@gmail.com>
2022-04-17 17:59:23 +0300

    final task is done

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,53 +1,22 @@
-// Исправьте функцию sendRequest
-// Аргументы функции:
-// - имя клиента
-// - телефон клиента
-// - объект с адресом доставки: {street, house, entrance, floor, flat}
-// - список товаров в заказе
-// - стоимость заказа с учетом скидок и доставки
-// Как результат функции требуется вернуть JSON,
-// cформированный в соответствии с правилами:
-// Объект data содержит все данные
-// В объекте data есть свойства:
-// - client - строка, имя клиента + телефон клиента;
-// - order - объект, содержащий данные о заказе:
-//     - address - строка с адресом доставки, записанным человекопонятным языком (как в примере)
-//     - sum - стоимость заказа с учетом скидок и доставки
-// - goods: массив объектов с информацией о позициях заказа:
-//     - title - название позиции
-//     - count - количество в заказе
-// например:
-// {
-//    "data": {
-//      "client": "Иван +7(987)65-43-210",
-//      "order": {
-//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
-//        "sum": 900
-//       },
-//       "goods": [
-//         {
-//           "title": "Пицца",
-//           "count": 2
-//         }
-//      ]
-//    }
-// }
-
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let data = {
+        client: `${name} ${phone}`, //строка, имя клиента + телефон
+        order: { //объект, содержащий данные о заказе
+            //строка с адресом доставки н-р: "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53"
+            address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`,
+            sum: sum, //стоимость заказа с учетом скидок и доставки
+        },
+        goods: [], //массив объектов с информацией о позициях заказа
+    };
+   
+    for (let i = 0; i < goods.length; i++) { 
+        data.goods[i] = { //массив объектов с информацией о позициях заказа
+            title: goods[i].title, //title - название позиции
+            count: goods[i].count, //count - количество в заказе
+        };
+    };
+    
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
